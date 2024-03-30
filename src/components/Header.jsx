7ba12a9a5eaa9738d1ef05c65b06efe3227e7b53
Mukesh kumar <122suraj0@gmail.com>
2024-03-30 19:59:09 +0530

    add logic to close filter card if cliked outside

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import FilterBy from "./FilterBy";
 import { IoIosList, IoIosArrowDown } from "react-icons/io";
 
 const Header = ({ groupBy, setGroupBy, sortBy, setSortBy }) => {
   const [isFilterCardhidden, setIsFilterCardHidden] = useState(false);
+  const filterCardRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        filterCardRef.current &&
+        !filterCardRef.current.contains(event.target)
+      ) {
+        setIsFilterCardHidden(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [filterCardRef]);
+
   return (
     <header className="header">
       <div className="container">
@@ -13,6 +31,7 @@ const Header = ({ groupBy, setGroupBy, sortBy, setSortBy }) => {
           <IoIosArrowDown />
         </button>
         <div
+          ref={filterCardRef}
           className={`filter-card ${isFilterCardhidden ? "visible" : "hidden"}`}
         >
           <FilterBy
